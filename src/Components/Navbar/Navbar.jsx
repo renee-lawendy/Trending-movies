fe591406f6d87decf991553cb8bd9200f3de23bc
Renee Lawendy <62428850+renee-lawendy@users.noreply.github.com>
2023-07-27 19:40:54 +0200

    deleting Component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import BreadCrumb from '../BreadCrumb/BreadCrumb';
+
 
 export default function Navbar({ userData, setUserData }) {
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js.map';
 import '@fortawesome/fontawesome-free/css/all.min.css';
